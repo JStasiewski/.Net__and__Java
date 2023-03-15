c9c6b5a7494659d835c4b4ce1268c4ca6105ec6e
stasiewski.jakub <stasiewski.jakub@gmail.com>
2023-03-15 16:03:51 +0100

    creatin desktop app version of backpack problem

---
+++ Lab02/Lab02/Backpack.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02
+{
+    internal class Backpack
+    {
+        int capacity = 30;
+        int priceInBackpack = 0;
+        string itemsId = "";
+        bool full = false;
+        
+        public void setCapacity(int capacity)
+        {
+            this.capacity = capacity;
+        }
+        public int Capacity()
+        { 
+            return capacity;
+        }
+        public int PriceInBackpack()
+        {
+            return priceInBackpack;
+        }
+        public string ItemsId()
+        {
+            return itemsId;
+        }
+        public void addToBackpack(int weight,int price, int id)
+        {
+            if (!full)
+            {
+                if (capacity - weight > 0)
+                {
+                    capacity -= weight;
+                    priceInBackpack += price;
+                    itemsId += id.ToString() + "   ";
+                }
+                else if(capacity - weight == 0)
+                {
+                    capacity -= weight;
+                    priceInBackpack += price;
+                    itemsId += id.ToString() + "   ";
+                    full = true;
+                }
+            }
+        }
+
+        public bool isFull()
+        {
+            return full;
+        }
+    }
+}
---
+++ Lab02/Lab02/Item.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02
+{
+    internal class Item
+    {
+        Random rnd = new Random();
+
+        int weight;
+        int price;
+        int id;
+
+        public Item() 
+        {
+            weight = rnd.Next(1,30);
+            price = rnd.Next(1,30);
+        }
+        public int getWeight()
+        {
+            return weight;
+        }
+
+        public int getPrice()
+        {
+            return price; 
+        }
+        public void setId(int id)
+        {
+            this.id = id;
+        }
+        public int getId()
+        {
+            return id;
+        }
+    }
+}

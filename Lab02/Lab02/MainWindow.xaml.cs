bd7cb92d7e4905093c42bf7606fb4c364a74556f
stasiewski.jakub <stasiewski.jakub@gmail.com>
2023-03-15 16:48:04 +0100

    printing results

--- Lab02/Lab02/MainWindow.xaml.cs
+++ Lab02/Lab02/MainWindow.xaml.cs
@@ -20,9 +20,60 @@ namespace Lab02
     /// </summary>
     public partial class MainWindow : Window
     {
+        static void Naive(List<Item> lits, Backpack backpack)
+        {
+            foreach (var item in lits)
+            {
+                if (!backpack.isFull())
+                {
+                    backpack.addToBackpack(item.getWeight(), item.getPrice(), item.getId());
+                }
+                else
+                {
+                    break;
+                }
+            }
+            //Console.WriteLine("Price colected : " + backpack.PriceInBackpack());
+            //Console.WriteLine("Capacity : " + backpack.Capacity());
+            //Console.WriteLine("Items ID's : " + backpack.ItemsId());
+            
+        }
         public MainWindow()
         {
             InitializeComponent();
+            
+            Backpack backpack = new Backpack();
+            int itemQ = 10;
+
+            Console.WriteLine("Default Backpack capacity : " + backpack.Capacity() + "   Item quantity : " + itemQ);
+            Console.WriteLine("Set other capacity and items quantity? (Y/N)");
+            string choise = Console.ReadLine();
+            if (choise == "Y")
+            {
+                Console.WriteLine("Set custom backpack capacity:");
+                int customCapacity = Convert.ToInt32(Console.ReadLine());
+                backpack.setCapacity(customCapacity);
+                Console.WriteLine("Set custom items quantity:");
+                itemQ = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("----------------------");
+
+            List<Item> list = new List<Item>();
+
+            for (int i = 0; i < itemQ; i++)
+            {
+                Item item = new Item();
+                list.Add(item);
+                list[i].setId(i);
+                Console.WriteLine($"Weight: {list[i].getWeight(),2}" + $"   Price: {list[i].getPrice(),2}" + $"   ID: {list[i].getId(),2}");
+            }
+
+            Naive(list, backpack);
+            result1.Text = "Price colected : " + backpack.PriceInBackpack().ToString();
+            result2.Text = "Capacity left : " + backpack.Capacity().ToString();
+            result3.Text = "Taken items ID's : " + backpack.ItemsId().ToString();
+
         }
     }
 }

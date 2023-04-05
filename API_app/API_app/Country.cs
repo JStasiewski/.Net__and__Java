ffc338374cb36f8ce1cbd50c0450d80220866e70
Jakub <stasiewski.jakub@gmail.com>
2023-04-05 16:55:43 +0200

    created new project, build structure of project, added api to program, err handling with geting json. TO DO converting json, adding new spots of chosen countries

---
+++ API_app/API_app/Country.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_app
+{
+    internal class Country
+    {
+        public Name name { get; set; }
+        public string region { get; set; }
+        public string Anwser()
+        {
+            return $"{this.name.common}: {this.region}";
+        }
+
+        public Country()
+        {
+            this.name.common = "";
+            this.region = "";
+        }
+    }
+    internal class Name
+    {
+        public string common { get; set; }
+    }
+}
---
+++ API_app/API_app/CountryAPI.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace API_app
+{
+    /// <summary>
+    /// Interaction logic for CountryAPI.xaml
+    /// </summary>
+    public partial class CountryAPI : Window
+    {
+        public CountryAPI()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string json = await downloadData(TextBox.Text);
+            TextBlock.Text = json;
+
+
+        }
+
+        private async Task<string> downloadData(string name)
+        {
+            HttpClient client = new HttpClient();
+            string call = $"https://restcountries.com/v3.1/name/{name}";
+            HttpResponseMessage response = await client.GetAsync(call);
+            if (response.IsSuccessStatusCode)
+            {
+                string json = await client.GetStringAsync(call);
+                return json;
+            }
+            else
+            {
+                return "No country found. Ty again!";
+            }
+        }
+    }
+}

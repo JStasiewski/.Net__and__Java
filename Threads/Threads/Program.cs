a770bd910be609ace061fda205562478468c7af0
stasiewski.jakub <stasiewski.jakub@gmail.com>
2023-05-11 15:21:44 +0200

    ostatecznia wersja która dziaaaaałąąąąąaaaaa

--- Threads/Threads/Program.cs
+++ Threads/Threads/Program.cs
@@ -1,32 +1,45 @@
-﻿
-using System;
+﻿using System;
 using System.Diagnostics;
-using System.Threading.Tasks;
+using System.Threading;
 
-class KnapsackProblem
+class Program
 {
-    static int KnapsackWithoutThreads(int[] values, int[] weights, int capacity)
+    static void Main(string[] args)
     {
-        int n = values.Length;
-        int[,] K = new int[n + 1, capacity + 1];
+        int numThreads = 4;             // Liczba wątków
+        int iterationsPerThread = 10000;// Liczba iteracji na każdy wątek
+        double[] results = new double[numThreads]; // Tablica wyników dla każdego wątku
 
-        for (int i = 0; i <= n; i++)
+        Stopwatch sw = Stopwatch.StartNew();  // Uruchomienie stopera
+
+        Thread[] threads = new Thread[numThreads]; // Tablica wątków
+        for (int i = 0; i < numThreads; i++)
         {
-            for (int w = 0; w <= capacity; w++)
+            int threadIndex = i;    // Utworzenie lokalnej kopii indeksu wątku dla każdego wątku
+            int start = threadIndex * iterationsPerThread; // Początek przedziału iteracji dla wątku
+            int end = start + iterationsPerThread - 1;     // Koniec przedziału iteracji dla wątku
+            threads[threadIndex] = new Thread(() =>
             {
-                if (i == 0 || w == 0)
-                    K[i, w] = 0;
-                else if (weights[i - 1] <= w)
-                    K[i, w] = Math.Max(values[i - 1] + K[i - 1, w - weights[i - 1]], K[i - 1, w]);
-                else
-                    K[i, w] = K[i - 1, w];
-            }
+                for (int j = start; j <= end; j++)
+                {
+                    results[threadIndex] += 1 / Factorial(j); // Obliczenie wyniku dla danego wątku
+                }
+            });
+            threads[threadIndex].Start(); // Uruchomienie wątku
         }
         
 
         return K[n, capacity];
     }
 
+        double eulerNumber = 0;  // Obliczenie ostatecznego wyniku z wyników każdego wątku
+        foreach (double result in results)
+        {
+            eulerNumber += result;
+        }
+        sw.Stop();  // Zatrzymanie stopera
+        Console.WriteLine("Euler number using threads: " + eulerNumber); // Wyświetlenie wyniku
+        Console.WriteLine("Time taken using threads: " + sw.ElapsedMilliseconds + " ms"); // Wyświetlenie czasu wykonania
     //          Wersia z równoległym wykonywaniem ale na Parallel a nie Thread
     //static int KnapsackWithThreads(int[] values, int[] weights, int capacity)
     //{
@@ -83,19 +96,17 @@ class KnapsackProblem
                 
             });
 
-            threads.Add(thread);
-            thread.Start();
-        }
+        sw = Stopwatch.StartNew(); // Uruchomienie stopera
 
-        foreach (Thread thread in threads)
+        double eulerNumber2 = 0; // Obliczenie wyniku bez użycia wątków
+        for (int j = 0; j < numThreads * iterationsPerThread; j++)
         {
-            thread.Join();
+            eulerNumber2 += 1 / Factorial(j);
         }
 
-       
-
-
-        return K[n, capacity];
+        sw.Stop(); // Zatrzymanie stopera
+        Console.WriteLine("Euler number without threads: " + eulerNumber2); // Wyświetlenie wyniku
+        Console.WriteLine("Time taken without threads: " + sw.ElapsedMilliseconds + " ms"); // Wyświetlenie czasu wykonania
     }
 
 
@@ -109,10 +120,10 @@ class KnapsackProblem
         int[] weights = new int[n];
         Random rnd = new Random();
 
-        for (int i = 0; i < n; i++)
+        for (int i = 2; i <= n; i++)
         {
-            values[i] = rnd.Next(1, 11);
-            weights[i] = rnd.Next(1, 11);
+            values[i] = rnd.Next(1, 101);
+            weights[i] = rnd.Next(1, 101);
         }
 
         Stopwatch stopwatch = new Stopwatch();
